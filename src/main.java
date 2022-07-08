1d33b10b00301b0b483d738b16f92150383c5687
Dasha Fedy <dashaafedy@gmail.com>
2022-07-08 14:15:27 +0300

    add changes

--- src/main.java
+++ src/main.java
@@ -1,15 +1,15 @@
 public class main {
     public static void main(String[] args) {
         int amount = 1100; // клиент пополнил счет
-        boolean payment = true;
+        boolean isPaid = true;
 
-        int bonus = payment ? 100 : 0;
-        int x = amount / bonus;
+        int bonus = isPaid ? 100 : 0;
+        int account = amount / bonus;
 
 
-        System.out.println( " Итоговый счет = " + (amount + bonus + x) );
-        System.out.println(" Бонус = " + (x));
+        System.out.println(" Итоговый счет = " + (amount + bonus + account));
+        System.out.println(" Бонус = " + (account));
 
-        }
     }
+}
 
